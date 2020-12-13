1f2c0aab5bba5900a97faf143bf92a813b17c9b4
ElenaMorozzova <ja-moroz@mail.ru>
2020-12-13 17:23:36 +0300

    dz5

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -1,8 +1,3 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
@@ -10,3 +5,29 @@ class GbParseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class AutoYoulaItem(scrapy.Item):
+    _id = scrapy.Field()
+    title = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    url = scrapy.Field()
+    autor = scrapy.Field()
+    specifications = scrapy.Field()
+
+
+class HhruItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    salary = scrapy.Field()
+    description = scrapy.Field()
+    skills = scrapy.Field()
+    vacancy_author_url = scrapy.Field()
+    company_name = scrapy.Field()
+    company_website = scrapy.Field()
+    company_description = scrapy.Field()
+    company_tags = scrapy.Field()
+
+
